19d635c4fb900a54183bc333b763280ad51513b2
Per-Kristian Helland <per-kristian.helland@komplett.com>
2021-01-02 14:54:04 +0100

    There should be some rudimentary unhandled error handling in place so that it's possible to debug eventual issues.

--- GeoTourney/Program.cs
+++ GeoTourney/Program.cs
@@ -8,108 +8,125 @@ using GeoTourney;
 using Microsoft.Extensions.Configuration;
 using PuppeteerSharp;
 
-/// <summary>
-/// https://www.geoguessr.com/challenge/4AIgheuBiawHesVD
-/// https://www.geoguessr.com/challenge/kdrp4V1ByTC2D7Qr
-/// </summary>
-GeoTournament tournament = new();
-var config = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json").Build();
-Console.OutputEncoding = Encoding.Unicode;
-IGameEventOutput[] possibleOutputs =
+// https://www.geoguessr.com/challenge/4AIgheuBiawHesVD
+// https://www.geoguessr.com/challenge/kdrp4V1ByTC2D7Qr
+try
 {
-    new TwitchClient(),
-    new ConsoleOutput(),
-    new FileOutput()
-};
-var allOutputs = possibleOutputs.Select(x => new
-{
-    Status = x.Initialize(config, OnMessageReceived),
-    Output = x
-}).ToList();
-var activeOutputs = allOutputs.Where(x => x.Status == InitializationStatus.Ok).Select(x => x.Output).ToList();
-var gihubAccess = await Github.VerifyGithubTokenAccess(config);
-if (!gihubAccess.hasAccess)
-{
-    Console.WriteLine(gihubAccess.errorMessage);
-    return;
+    GeoTournament tournament = new();
+    var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json").Build();
+    Console.OutputEncoding = Encoding.Unicode;
+    IGameEventOutput[] possibleOutputs =
+    {
+        new TwitchClient(),
+        new ConsoleOutput(),
+        new FileOutput()
+    };
+    var allOutputs = possibleOutputs.Select(x => new
+    {
+        Status = x.Initialize(config, OnMessageReceived),
+        Output = x
+    }).ToList();
+    var activeOutputs = allOutputs.Where(x => x.Status == InitializationStatus.Ok).Select(x => x.Output).ToList();
+    var gihubAccess = await Github.VerifyGithubTokenAccess(config);
+    if (!gihubAccess.hasAccess)
+    {
+        Console.WriteLine(gihubAccess.errorMessage);
+        Console.ReadKey();
+        return;
+    }
+
+    var localExampleTournamentPath = config["LocalExampleTournamentPath"];
+    if (!string.IsNullOrEmpty(localExampleTournamentPath) && File.Exists(localExampleTournamentPath))
+    {
+        var url = await Github.UploadTournamentData(config, await File.ReadAllTextAsync(localExampleTournamentPath));
+        await Clip.SetText(url);
+    }
+
+    await BrowserSetup.Initiate();
+    var browser = await Puppeteer.LaunchAsync(BrowserSetup.LaunchOptions);
+    var page = await browser.NewPageAsync();
+    await page.GoToAsync("https://www.geoguessr.com/signin");
+    GeoTournament.PrintCommands();
+    Console.WriteLine();
+
+    while (true)
+    {
+        var inputCommand = ReadConsole.ReadLine(TimeSpan.FromSeconds(10));
+        if (inputCommand?.Equals("exit", StringComparison.OrdinalIgnoreCase) ?? false)
+        {
+            try
+            {
+                await page.CloseAsync();
+                await browser.CloseAsync();
+            }
+            catch (Exception)
+            {
+                // Can't error on shutdown.
+            }
+
+            break;
+        }
+
+        if (inputCommand == null)
+        {
+            foreach (var output in activeOutputs) output.KeepAlive();
+
+            var messageToChat = await tournament.CheckIfCurrentGameFinished(page, config);
+            if (messageToChat != null)
+            {
+                WriteOutput(activeOutputs, messageToChat);
+            }
+        }
+        else if (Uri.TryCreate(inputCommand, UriKind.Absolute, out var uri))
+        {
+            await tournament.SetCurrentGame(uri, page, config);
+            var currentGameNumber = (tournament.Games.OrderByDescending(x => x.GameNumber).FirstOrDefault()?.GameNumber ?? 0) + 1;
+            WriteOutput(activeOutputs, $"Game #{currentGameNumber}: {uri}");
+        }
+        else if (inputCommand == "restart")
+        {
+            tournament = tournament.Restart();
+        }
+        else if (inputCommand == "gamescore")
+        {
+            var url = await tournament.PrintGameScore(config);
+            WriteOutput(activeOutputs, $"Last game results: {url}");
+        }
+        else if (inputCommand == "totalscore")
+        {
+            var url = await tournament.PrintTotalScore(config);
+            WriteOutput(activeOutputs, $"Tournament results: {url}");
+        }
+        else if (inputCommand == "elim")
+        {
+            var message = await tournament.ToggleEliminations(page, config);
+            WriteOutput(activeOutputs, $"Eliminations are now {(tournament.PlayWithEliminations ? "ON" : "OFF")}.");
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            if(message != null) WriteOutput(activeOutputs, message);
+        }
+        else if (inputCommand.StartsWith("eliminate"))
+        {
+            var numberOfEliminations = int.TryParse(inputCommand.Split(' ').LastOrDefault(), out var num) ? num : 0;
+            var messageToChat = await tournament.EliminateAndFinish(page, config, numberOfEliminations);
+            if (messageToChat != null) WriteOutput(activeOutputs, messageToChat);
+        }
+    }
 }
-
-var localExampleTournamentPath = config["LocalExampleTournamentPath"];
-if (!string.IsNullOrEmpty(localExampleTournamentPath) && File.Exists(localExampleTournamentPath))
+catch (Exception e)
 {
-    var url = await Github.UploadTournamentData(config, await File.ReadAllTextAsync(localExampleTournamentPath));
-    await Clip.SetText(url);
-}
-
-await BrowserSetup.Initiate();
-var browser = await Puppeteer.LaunchAsync(BrowserSetup.LaunchOptions);
-var page = await browser.NewPageAsync();
-await page.GoToAsync("https://www.geoguessr.com/signin");
-GeoTournament.PrintCommands();
-Console.WriteLine();
-while (true)
-{
-    var inputCommand = ReadConsole.ReadLine(TimeSpan.FromSeconds(10));
-    if (inputCommand?.Equals("exit", StringComparison.OrdinalIgnoreCase) ?? false)
+    Console.WriteLine(e);
+    Console.WriteLine("An unexpected error occurred.");
+    Console.WriteLine("The application will stop and needs to be started again. Press any key to close.");
+    try
     {
-        try
-        {
-            await page.CloseAsync();
-            await browser.CloseAsync();
-        }
-        catch (Exception)
-        {
-            // Can't error on shutdown.
-        }
-
-        break;
+        await File.AppendAllTextAsync("errors.txt", $"{DateTime.UtcNow:s}: {e}{Environment.NewLine}");
     }
-
-    if (inputCommand == null)
+    catch (Exception)
     {
-        foreach (var output in activeOutputs) output.KeepAlive();
-
-        var messageToChat = await tournament.CheckIfCurrentGameFinished(page, config);
-        if (messageToChat != null)
-        {
-            WriteOutput(activeOutputs, messageToChat);
-        }
     }
-    else if (Uri.TryCreate(inputCommand, UriKind.Absolute, out var uri))
-    {
-        await tournament.SetCurrentGame(uri, page, config);
-        var currentGameNumber = (tournament.Games.OrderByDescending(x => x.GameNumber).FirstOrDefault()?.GameNumber ?? 0) + 1;
-        WriteOutput(activeOutputs, $"Game #{currentGameNumber}: {uri}");
-    }
-    else if (inputCommand == "restart")
-    {
-        tournament = tournament.Restart();
-    }
-    else if (inputCommand == "gamescore")
-    {
-        var url = await tournament.PrintGameScore(config);
-        WriteOutput(activeOutputs, $"Last game results: {url}");
-    }
-    else if (inputCommand == "totalscore")
-    {
-        var url = await tournament.PrintTotalScore(config);
-        WriteOutput(activeOutputs, $"Tournament results: {url}");
-    }
-    else if (inputCommand == "elim")
-    {
-        var message = await tournament.ToggleEliminations(page, config);
-        WriteOutput(activeOutputs, $"Eliminations are now {(tournament.PlayWithEliminations ? "ON" : "OFF")}.");
-        await Task.Delay(TimeSpan.FromSeconds(2));
-        if(message != null) WriteOutput(activeOutputs, message);
-    }
-    else if (inputCommand.StartsWith("eliminate"))
-    {
-        var numberOfEliminations = int.TryParse(inputCommand.Split(' ').LastOrDefault(), out var num) ? num : 0;
-        var messageToChat = await tournament.EliminateAndFinish(page, config, numberOfEliminations);
-        if (messageToChat != null) WriteOutput(activeOutputs, messageToChat);
-    }
+    Console.ReadKey();
 }
 
 static void OnMessageReceived(object? sender, string e)
